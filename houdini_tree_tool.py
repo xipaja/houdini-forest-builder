068b14908e140710fbbbfabcdd7b702750afaa5b
xipaja <xipaja@gmail.com>
2022-05-15 23:47:00 -0700

    Adding decorator and config to tool

--- houdini_tree_tool.py
+++ houdini_tree_tool.py
@@ -1,47 +1,33 @@
+from email.mime import base
 import hou 
+import json 
 
-def createCurve():
-    
-    # Optional TOOOs
-    # if not exist, create
-    # from.to architecture instead of Houdini's to.from (builder) for setInput 
-    # translation layer for (0, p_curve1, 0) - what are the 0 inputs? Add useful var names for user
-    # spread out network view - not have to manually drag the nodes around to see them all
+json_path = "C:\Users\xipaj\Desktop\python_advanced_tool\python-advanced-tool"
 
-    # TODO: make curve NURBS curve - parm edit
-    # When you build UI, have a slider or text input for number of trees you want to do
-    # ^ corresponds to 'length' parm of resample node
-    # UI - slider to adjust density of trees
-        # create grouprange node, connect it to chosen shape ('torus', 'curve', whatever user chooses)
-        # change Range Filter Select # of # (second parm - 'selecttotal1') based on UI slider
-        # In copytopoints node, change Target Points parm ('targetgroup') to be the name of group in 
-            # the grouprange node ('groupname1')
-            
-    curve_obj = hou.node('/obj').createNode('geo', 'p_curve_objects')
-    p_curve1 = curve_obj.createNode('curve', 'p_curve1')
-    
-    p_resample = curve_obj.createNode('resample', 'p_resample')
-    p_resample.setInput(0, p_curve1, 0)
-    
-    p_copy_to_points = curve_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
-    p_copy_to_points.setInput(1, p_resample, 0)
-    
-    p_lsystem = curve_obj.createNode('lsystem', 'p_lsystem')
-    p_copy_to_points.setInput(0, p_lsystem, 0)
-    p_copy_to_points.setDisplayFlag(True)
-    
-    # geo_node = Node(parent_node=obj, node_type='geo', given_name='geo_node')
-    # geo_node.createNode()
-    
-    # curve_node = CurveNode(geo_node, 'curve', 'curve1', 3)
-    # curve_node.createNode()
-    
-    # tree_node = geo_node.createNode('lsystem', 'tree')
-    # tree_node.setDisplayFlag(1)
-    
+base_shape_data = {
+    'Torus SOP': 'torus',
+    'Box SOP': 'box',
+    'Grid SOP': 'grid',
+    'Draw Curve SOP': 'drawcurve',
+    'Curve SOP': 'curve'
+}
+
+"""
+This config will help with the UI later, as I plan to give the user options of shapes they can use to
+populate trees on top of
+"""
+with open(json_path, 'w') as outFile:
+    json.dump(base_shape_data, outFile, indent=4)
+
 class Node:
     """
     Node class for creating any kind of node in Houdini
+
+    Args:
+        parent_node(hou node): parent_node this node will be nested in
+        node_type(str): Houdini node type for this node
+        given_name(str): User-given name for this node
+        display(bool): Displays this node (or not)
     """
 
     def __init__(self, parent_node, node_type, given_name, display=True):
@@ -53,18 +39,35 @@ class Node:
         self.parent_node.createNode(self.node_type, self.given_name)
         # new_node.setDisplayFlag(self.display)
 
-    # def createNode(self):
-    #     new_node = self.parent_node.createNode(self.node_type, self.name)
-    #     new_node.setDisplayFlag(self.display)
+    def print_node_status(self, incoming_func):
+        def wrapper(*args):
+            print('Creating Node...')
+            incoming_func(args)
+            print(f'Node {self.given_name} of type {self.node_type} created!')
+        return wrapper
 
-    #     return new_node
-    
-    def connectNodes(self, child_node):
+    @print_node_status
+    def create_node(self):
+        new_node = self.parent_node.createNode(self.node_type, self.name)
+        new_node.setDisplayFlag(self.display)
+
+        return new_node
+
+    def connect_nodes(self, child_node):
         self.parent_node.setInput(0, child_node, 0)
 
 class CurveNode(Node):
     """
     Curve class, derived from Node class to have node data and also custom Curve data 
+
+    Args:
+        parent_node(hou node): parent_node this node will be nested in
+        node_type(str): Houdini node type for this node
+        given_name(str): User-given name for this node
+        num_points(int): Num of points for this node - will eventually be a slider in UI
+
+    This is useful as a class because I would like it to be like the Node class, but have
+    its own custom values, such as the number of points (entered by user)
     """
     
     def __init__(self, parent_node, node_type, given_name, num_points):
@@ -78,10 +81,40 @@ class CurveNode(Node):
         # Set param 'Order' to user-determined num_points
         new_node.parm('order').set(self.num_points)
 
-        # Set curve to be NURBS curve for ease of use
-        new_node.parm('outputtype').set('NURBS Curve')
+        # Set curve to be Bezier curve for ease of use
+        new_node.parm('outputtype').set('Bezier Curve')
 
+def main():
+    obj = hou.node('/obj')
 
-def parentLSystem(parent_node, child_node):
-    parent_node.setInput(0, child_node,0)     
- 
+    curve_node = CurveNode(parent_node=obj, node_type='curve', given_name='p_curve', num_points=4)
+    
+    # curve_obj = hou.node('/obj').createNode('geo', 'p_curve_objects')
+    # p_curve1 = curve_obj.createNode('curve', 'p_curve1')
+    
+    # p_resample = curve_obj.createNode('resample', 'p_resample')
+    # p_resample.setInput(0, p_curve1, 0)
+    
+    # p_copy_to_points = curve_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
+    # p_copy_to_points.setInput(1, p_resample, 0)
+    
+    # p_lsystem = curve_obj.createNode('lsystem', 'p_lsystem')
+    # p_copy_to_points.setInput(0, p_lsystem, 0)
+    # p_copy_to_points.setDisplayFlag(True)
+
+    # Upcoming TOOOs
+        # if not exist, create
+        # from.to architecture instead of Houdini's to.from (builder) for setInput 
+        # translation layer for (0, p_curve1, 0) - what are the 0 inputs? Add useful var names for user
+        # spread out network view - not have to manually drag the nodes around to see    them all
+
+    # UI TODOs
+    # have a slider or text input for number of trees you want to do
+        # ^ corresponds to 'length' parm of resample node
+        #   if user chosen geo is 1D (curve, etc), add resample node between geo and copy to pts nodes
+    # slider to adjust density of trees
+        # create grouprange node, connect it to chosen shape ('torus', 'curve', whatever user chooses)
+        # change Range Filter Select # of # (second parm - 'selecttotal1') based on UI slider
+        # In copytopoints node, change Target Points parm ('targetgroup') to be the name of group in 
+            # the grouprange node ('groupname1')
+        # Make sure grouprange Group Type is 'Points' ('grouptype1')
