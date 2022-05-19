51e1805e66ec80f417d9d8b4b1d59e340a371a4b
xipaja <xipaja@gmail.com>
2022-05-19 00:03:42 -0700

    Adding dropdown functionality, now works with creating new geos with trees!

--- houdini_tree_tool.py
+++ houdini_tree_tool.py
@@ -1,109 +1,73 @@
+from platform import node
 import hou
 from PySide2 import QtCore, QtUiTools, QtWidgets
 
 class ForestCreator(QtWidgets.QWidget):
+    
     def __init__(self):
         super(ForestCreator,self).__init__()
         ui_file = 'C:/hou_temp/uiForestCreator.ui'
         self.ui = QtUiTools.QUiLoader().load(ui_file, parentWidget=self)
         self.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
-        
+
+        self.user_selected_geo = 'curve'
+
         # SIGNALS
-        # Setup button
+        self.ui.dropdown.activated.connect(self.selectionChange)
         self.ui.btn_create.clicked.connect(self.btnClicked)
+
+    def selectionChange(self, user_selection):
+        print("user selection :", user_selection)
         
+        dropdown_options = {
+            0 : 'Curve',
+            1 : 'Box',
+            2 : 'Sphere',
+            3 : 'Tube',
+            4 : 'Torus',
+            5 : 'Grid'
+        }
+        
+        # Convert user selection to lowercase to match Houdini params
+        self.user_selected_geo = dropdown_options[user_selection].lower() 
+        
+        return str(self.user_selected_geo)
+
     def btnClicked(self):
-        curve_obj = hou.node('/obj').createNode('geo', 'p_curve_objects')
-        p_curve1 = curve_obj.createNode('curve', 'p_curve1')
+
+        base_obj = hou.node('/obj').createNode('geo', f'p_{self.user_selected_geo}_object')
+        p_base_geo = base_obj.createNode(self.user_selected_geo, f'p_{self.user_selected_geo}')
     
-        p_resample = curve_obj.createNode('resample', 'p_resample')
-        p_resample.setInput(0, p_curve1, 0)
+        # p_resample = base_obj.createNode('resample', 'p_resample')
+        # p_resample.setInput(0, p_base_geo, 0)
     
-        p_copy_to_points = curve_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
+        # p_copy_to_points = base_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
+        # p_copy_to_points.setInput(1, p_resample, 0)
+    
+        # p_lsystem = base_obj.createNode('lsystem', 'p_lsystem')
+        # p_copy_to_points.setInput(0, p_lsystem, 0)
+        # p_copy_to_points.setDisplayFlag(True)
+
+
+        p_resample = base_obj.createNode('resample', 'p_resample')
+        p_resample.setInput(0, p_base_geo, 0)
+
+        if self.user_selected_geo != 'curve':
+            p_connector = base_obj.createNode('grouprange', 'p_grouprange')
+            p_connector.setInput(0, p_resample, 0)
+
+        p_copy_to_points = base_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
         p_copy_to_points.setInput(1, p_resample, 0)
     
-        p_lsystem = curve_obj.createNode('lsystem', 'p_lsystem')
+        p_lsystem = base_obj.createNode('lsystem', 'p_lsystem')
         p_copy_to_points.setInput(0, p_lsystem, 0)
         p_copy_to_points.setDisplayFlag(True)
+
 
 win = ForestCreator()
 win.show()
 
-class Node:
-    """
-    Node class for creating any kind of node in Houdini
-
-    Args:
-        parent_node(hou node): parent_node this node will be nested in
-        node_type(str): Houdini node type for this node
-        given_name(str): User-given name for this node
-        display(bool): Displays this node (or not)
-    """
-
-    def __init__(self, parent_node, node_type, given_name, display=True):
-        self.parent_node = parent_node
-        self.node_type = node_type
-        self.given_name = given_name
-        self.display = display
-
-        self.parent_node.createNode(self.node_type, self.given_name)
-        # new_node.setDisplayFlag(self.display)
-
-    def print_node_status(self, incoming_func):
-        def wrapper(*args):
-            print('Creating Node...')
-            incoming_func(args)
-            print(f'Node {self.given_name} of type {self.node_type} created!')
-        return wrapper
-
-    @print_node_status
-    def create_node(self):
-        new_node = self.parent_node.createNode(self.node_type, self.name)
-        new_node.setDisplayFlag(self.display)
-
-        return new_node
-
-    def connect_nodes(self, child_node):
-        self.parent_node.setInput(0, child_node, 0)
-
-class CurveNode(Node):
-    """
-    Curve class, derived from Node class to have node data and also custom Curve data 
-
-    Args:
-        parent_node(hou node): parent_node this node will be nested in
-        node_type(str): Houdini node type for this node
-        given_name(str): User-given name for this node
-        num_points(int): Num of points for this node - will eventually be a slider in UI
-
-    This is useful as a class because I would like it to be like the Node class, but have
-    its own custom values, such as the number of points (entered by user)
-    """
-    
-    def __init__(self, parent_node, node_type, given_name, num_points):
-        super().__init__(parent_node, node_type, given_name)
-
-        self.num_points = num_points
-
-        new_node = self.createNode(self.parent_node, self.node_type, self.given_name)
-        new_node.setDisplayFlag(self.display)
-
-        # Set param 'Order' to user-determined num_points
-        new_node.parm('order').set(self.num_points)
-
-        # Set curve to be Bezier curve for ease of use
-        new_node.parm('outputtype').set('Bezier Curve')
-
-    # If exists, make a new curve with name prev + 1 - string interp
-
-    # Upcoming TOOOs
-        # if not exist, create
-        # from.to architecture instead of Houdini's to.from (builder) for setInput 
-        # translation layer for (0, p_curve1, 0) - what are the 0 inputs? Add useful var names for user
-        # spread out network view - not have to manually drag the nodes around to see    them all
-
-    # UI TODOs
-    # have a slider or text input for number of trees you want to do
+ # have a slider or text input for number of trees you want to do
         # ^ corresponds to 'length' parm of resample node
         #   if user chosen geo is 1D (curve, etc), add resample node between geo and copy to pts nodes
     # slider to adjust density of trees
