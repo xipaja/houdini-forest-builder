7ce3fe0ca385bd9d645c85d0e15124f997e8e9ca
xipaja <xipaja@gmail.com>
2022-05-19 17:17:51 -0700

    Currently works to switch geo and create new node tree

--- houdini_tree_tool.py
+++ houdini_tree_tool.py
@@ -38,20 +38,10 @@ class ForestCreator(QtWidgets.QWidget):
         base_obj = hou.node('/obj').createNode('geo', f'p_{self.user_selected_geo}_object')
         p_base_geo = base_obj.createNode(self.user_selected_geo, f'p_{self.user_selected_geo}')
     
-        # p_resample = base_obj.createNode('resample', 'p_resample')
-        # p_resample.setInput(0, p_base_geo, 0)
-    
-        # p_copy_to_points = base_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
-        # p_copy_to_points.setInput(1, p_resample, 0)
-    
-        # p_lsystem = base_obj.createNode('lsystem', 'p_lsystem')
-        # p_copy_to_points.setInput(0, p_lsystem, 0)
-        # p_copy_to_points.setDisplayFlag(True)
-
-
         p_resample = base_obj.createNode('resample', 'p_resample')
         p_resample.setInput(0, p_base_geo, 0)
 
+        # If a 3D geo, need to add a grouprange node for editing forest density
         if self.user_selected_geo != 'curve':
             p_connector = base_obj.createNode('grouprange', 'p_grouprange')
             p_connector.setInput(0, p_resample, 0)
@@ -72,7 +62,7 @@ win.show()
         #   if user chosen geo is 1D (curve, etc), add resample node between geo and copy to pts nodes
     # slider to adjust density of trees
         # create grouprange node, connect it to chosen shape ('torus', 'curve', whatever user chooses)
+        # Make sure grouprange Group Type is 'Points' ('grouptype1')
         # change Range Filter Select # of # (second parm - 'selecttotal1') based on UI slider
         # In copytopoints node, change Target Points parm ('targetgroup') to be the name of group in 
             # the grouprange node ('groupname1')
-        # Make sure grouprange Group Type is 'Points' ('grouptype1')
