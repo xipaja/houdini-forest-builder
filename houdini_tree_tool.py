47c268859734646d0f0c0409f29f7dbd09f0420f
xipaja <xipaja@gmail.com>
2022-05-23 19:53:24 -0700

    Clean up, move logic into functions, add comments throughout

--- houdini_tree_tool.py
+++ houdini_tree_tool.py
@@ -12,12 +12,21 @@ class ForestCreator(QtWidgets.QWidget):
 
         self.user_selected_geo = 'curve'
         self.slider_value = 1
+    
+        # Nodes
+        self.base_obj = None
+        self.base_geo = None
+        self.resample_node = None
+        self.copy_to_points_node = None
+        self.group_range_node = None
+        self.l_system_node = None
 
-        # SIGNALS
+        # Signals
         self.ui.dropdown.activated.connect(self.selectionChange)
         self.ui.btn_create.clicked.connect(self.btnClicked)
-        self.ui.slider_density.valueChanged.connect(self.sliderSlid)
+        self.ui.slider_density.valueChanged.connect(self.sliderChanged)
 
+    # -------------------------- UI methods --------------------------
     def selectionChange(self, user_selection):
         
         dropdown_options = {
@@ -36,60 +45,90 @@ class ForestCreator(QtWidgets.QWidget):
 
     def btnClicked(self):
 
-        base_obj = hou.node('/obj').createNode('geo', f'p_{self.user_selected_geo}_object')
-        p_base_geo = base_obj.createNode(self.user_selected_geo, f'p_{self.user_selected_geo}')
-    
-        self.p_resample = base_obj.createNode('resample', 'p_resample')
-        self.p_resample.setInput(0, p_base_geo, 0)
-
-        p_copy_to_points = base_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
+        self.setUpBaseNodes()
        
-       # If user selects curve, change length of resample node for changing tree density
+       # If user selects curve, change length of resample node for editing tree density
         if self.user_selected_geo == 'curve':
-            p_copy_to_points.setInput(1, self.p_resample, 0)
+            self.copy_to_points_node.setInput(1, self.resample_node, 0)
 
-        # If a 3D geo, need to add a grouprange node for editing forest density
+        # If a 3D geo, need to add a grouprange node instead of resample for editing forest density
         elif self.user_selected_geo != 'curve':
-            self.p_group_range = base_obj.createNode('grouprange', 'p_grouprange')
-            self.p_group_range.setInput(0, self.p_resample, 0)
-
-            p_copy_to_points.setInput(1, self.p_group_range, 0)            
-
-            # Setting up for changing tree density
-            group_name = self.p_group_range.parm('groupname1')
-            
-            # Set up group type to be 'Points' for copy to points node to adjust trees by points 
-            self.p_group_range.parm('grouptype1').set(0)         
-            p_copy_to_points.parm('targetgroup').set(group_name)
+            self.setUpGeoDensityNodes()
         
-        p_lsystem = base_obj.createNode('lsystem', 'p_lsystem')
-        p_copy_to_points.setInput(0, p_lsystem, 0)
-        p_copy_to_points.setDisplayFlag(True)
-
+        self.addTrees()        
+        
+        self.group_range_node.parm('selecttotal1').set(10)
+        
         # Layout network view in clean tree view
-        base_obj.moveToGoodPosition()
-        base_obj.layoutChildren()
+        self.base_obj.moveToGoodPosition()
+        self.base_obj.layoutChildren()
 
-    def sliderSlid(self):
+    def sliderChanged(self):
         self.slider_value = self.ui.slider_density.value()
 
         # Adjust tree density on slider change
         # Set select every 1 of {slider_value} points in geo
         if self.user_selected_geo == 'curve':
-            self.p_resample.parm('length').set(self.slider_value)
+            self.resample_node.parm('length').set(self.slider_value)
         else:
-            self.p_group_range.parm('selecttotal1').set(self.slider_value)
+            self.group_range_node.parm('selecttotal1').set(self.slider_value)
+    
+    # --------------------- Node operations methods ---------------------
+    def setUpBaseNodes(self):
+        '''
+         Sets up base nodes that all geos need 
+        '''
+        
+        # Outer geo nodes
+        self.base_obj = hou.node('/obj').createNode('geo', f'p_{self.user_selected_geo}_object')
+        self.base_geo = self.base_obj.createNode(self.user_selected_geo, f'p_{self.user_selected_geo}')
+    
+        # Resample to equal surface segments for cleaner distribution of trees later
+        self.resample_node = self.base_obj.createNode('resample', 'p_resample')
+        self.resample_node.setInput(0, self.base_geo, 0)
+        
+        # Houdini auto set up length of resulting resample - avoids accidental user error & visual bugs 
+        self.resample_node.parm('dolength').set(0)
+        
+        # Create copy_to_points node that will copy trees to geo points
+        self.copy_to_points_node = self.base_obj.createNode('copytopoints::2.0', 'p_copy_to_points')
+
+    def addTrees(self):
+        '''
+        Add the nodes necessary to add l-system "trees" to the geos
+        '''
+        self.l_system_node = self.base_obj.createNode('lsystem', 'p_lsystem')
+        self.copy_to_points_node.setInput(0, self.l_system_node, 0)
+        self.copy_to_points_node.setDisplayFlag(True)
+
+    def setUpGeoDensityNodes(self):
+        '''
+        Set up the nodes for changing the density for 3D non-curve geos
+
+        GR = Group Range
+        C2P = Copy to Points
+
+        This workflow in Houdini looks like:
+            - GR node connected to Resample output and C2P input
+            - GR "Group Name" = group1 (this is used later in workflow!)
+            - Group "Type" changed to Points (allows for C2P to work with it)
+            - GR "Range Filter" changes to "Select 1 of [slider value]", depending on user changed slider
+            - C2P "Target Points" set to 'group1' - aforementioned "Group Name"
+        '''
+
+        # Group range node groups points/prims by specified ranges
+        self.group_range_node = self.base_obj.createNode('grouprange', 'p_grouprange')
+        self.group_range_node.setInput(0, self.resample_node, 0)
+
+        self.copy_to_points_node.setInput(1, self.group_range_node, 0)            
+
+        group_name = self.group_range_node.parm('groupname1')
+            
+        # Set up group type to be 'Points'
+        self.group_range_node.parm('grouptype1').set(0)         
+        self.copy_to_points_node.parm('targetgroup').set(group_name)
+
+
 
 win = ForestCreator()
 win.show()
-
- # have a slider or text input for number of trees you want to do
-        # ^ corresponds to 'length' parm of resample node
-        #   if user chosen geo is 1D (curve, etc), add resample node between geo and copy to pts nodes
-    # slider to adjust density of trees
-        # create grouprange node, connect it to chosen shape ('torus', 'curve', whatever user chooses)
-        # Make sure grouprange Group Type is 'Points' ('grouptype1')
-        # change Range Filter Select # of # (second parm - 'selecttotal1') based on UI slider
-        # In copytopoints node, change Target Points parm ('targetgroup') to be the name of group in 
-            # the grouprange node ('groupname1')
- 
