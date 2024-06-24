b1a408f6a3dfb1f29cef8541fa5202b481a5f8bc
xipaja <xipaja@gmail.com>
2024-06-23 19:10:00 -0700

    Update generate_houdini_integration script

--- generate_houdini_integration.py
+++ generate_houdini_integration.py
@@ -3,13 +3,19 @@ Script to generate the Houdini integration code for the forest builder tool
 '''
 
 print('\n')
-print('------- Hello! Paste the following snippet into Houdini\'s tool script tab: ------- \n')
+
+print('Please enter the path to where you cloned/downloaded Forest Builder\n')
+print('For example: C:/Users/Ximena/Desktop/houdini-forest-builder\n\n')
+
+print('Enter path: ')
+user_path = input()
+
+print('\n')
+print('------- Paste the following snippet into Houdini\'s tool script tab: ------- \n')
 houdini_integration_script = f'''
 from importlib import reload
 import sys
-from pathlib import Path
-sys.path.append(Path.cwd())
-
+sys.path.append("{user_path.strip()}")
 import forest_builder
 reload(forest_builder)
 
